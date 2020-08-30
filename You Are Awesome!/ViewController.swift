9dac9137463e7693533aa4740c615763c1dedf49
Sangha Lee <seansanghalee@gmail.com>
2020-08-30 13:43:22 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Sangha Lee on 8/30/20.
+//  Copyright © 2020 Sangha Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
